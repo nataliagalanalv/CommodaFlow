cf345efa7e220c4007a60b99d7a520f5be47994a
Natalia Galan <nataliagalan@gamail.com>
2026-05-06 16:53:16 +0200

    clean

--- node_modules/commodaflow-server/src/routes/rentalRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { createRental } from '../controllers/rentalController.js';
-
-const router = Router();
-
-// POST /api/rentals
-router.post('/', createRental);
-
-export default router;

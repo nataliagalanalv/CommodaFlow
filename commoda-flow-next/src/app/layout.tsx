cbe617a89df044b1a74fde46fc1feaa8d964e8a2
Natalia Galan <nataliagalan@gamail.com>
2026-05-13 18:54:22 +0200

    fix (NavBar)

--- commoda-flow-next/src/app/layout.tsx
+++ commoda-flow-next/src/app/layout.tsx
@@ -3,12 +3,10 @@
 import './globals.css'
 import { useAuth } from '../hooks/useAuth'
 import { NavigationMain } from '../components/NavigationMain' 
-import { usePathname } from 'next/dist/client/components/navigation';
 
 // Creamos un componente interno para tener acceso al hook useAuth
 export function NavWrapper({ children }: { children: React.ReactNode }) {
   const { isAuthenticated, isLoading } = useAuth();
-  const pathname = usePathname();
 
   // 1. Mientras la sesión carga, solo mostramos el contenido (evita parpadeos)
   if (isLoading) return <>{children}</>;
